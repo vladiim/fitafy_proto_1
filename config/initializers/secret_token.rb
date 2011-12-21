8049b2d34a25ff86f30114488c55f985d6dde13c
Vlad Ivanovic <vladiim@me.com>
2011-12-21 19:41:18 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitafy::Application.config.secret_token = 'ac5dbca67e73e6ff56f6e614d7c6b476771c79fa08f7c19424354a674e42b21317bdb7a0015e0b8f09bb4b713b9da92f88c07b40afcba1989c95536dd6c29f6b'

c2d04f3ad1e409ba191224303b0a9142819e19e3
Vlad Ivanovic <vladiim@me.com>
2012-01-08 08:39:37 +1100

    added exercises to user_side_bar and added test for user_side_bar links

--- spec/requests/user_side_bars_spec.rb
+++ spec/requests/user_side_bars_spec.rb
@@ -15,6 +15,16 @@ describe "UserSideBars" do
     page.should have_content("#{@trainer.username}")
     page.should have_css("a", :text => "Clients: #{@trainer.training.count}")    
     page.should have_css("a", :text => "Bookings:")    
-    page.should have_css("a", :text => "Workouts: #{@trainer.workouts.count}")                    
+    page.should have_css("a", :text => "Workouts: #{@trainer.workouts.count}")   
+    page.should have_css("a", :text => "Exercises: #{Exercise.count}")                        
+  end
+  
+  it "should take users to the right path" do
+    click_link("Clients: #{@trainer.training.count}")
+    current_path.should eq(training_user_path(@trainer))    
+    click_link("Workouts: #{@trainer.workouts.count}")
+    current_path.should eq(workouts_path(@trainer))
+    click_link("Exercises: #{Exercise.count}")
+    current_path.should eq(exercises_path)
   end
 end

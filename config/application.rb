b9b25793c6499fbb87770a406132b2835b0e8f8d
Vlad Ivanovic <vladiim@me.com>
2012-01-27 14:21:31 +1100

    revised config/application.rb to fix issue with heroku trying to connect to the db during asset:precompile, line added: config.assets.initialize_on_precompile = false

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module Fitafy
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Stop connection to the database during precompile, heroku issue with rails 3.2
+    config.assets.initialize_on_precompile = false
   end
 end

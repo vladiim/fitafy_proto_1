8a43ead8472e5cce42dc10099d959a46cf1bfdea
Vlad Ivanovic <vladiim@me.com>
2012-02-13 17:15:59 +1100

    added invite_id to model/user.rb attr_accessible

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   
-  attr_accessible :username, :email, :password, :password_confirmation, :role
+  attr_accessible :username, :email, :password, :password_confirmation, :role, :invitation_id
   
   acts_as_authentic 
   

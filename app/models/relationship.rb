26dc06d1cf4e77c8b3aab87e243e12a3cd627ae4
Vlad Ivanovic <vladiim@me.com>
2012-02-17 21:47:12 +1100

    added relationship.declined which destroys the relationship if the client declines the invite

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,6 +1,6 @@
 class Relationship < ActiveRecord::Base
   
-  attr_accessible :client_id, :accepted
+  attr_accessible :client_id, :accepted, :declined
   
   belongs_to :trainer, :class_name => "User"
   belongs_to :client, :class_name => "User"
@@ -12,10 +12,15 @@ class Relationship < ActiveRecord::Base
   scope :unaccepted, where(:accepted => false)  
   
   after_create :send_client_invite
+  after_save :destroy_if_declined
   
   protected
   
     def send_client_invite
       UserMailer.send_client_invite(self).deliver
+    end
+    
+    def destroy_if_declined
+      destroy if declined?
     end
 end
--- db/migrate/20120216062224_add_accepted_to_relationship.rb
+++ db/migrate/20120216062224_add_accepted_to_relationship.rb
@@ -1,6 +1,6 @@
 class AddAcceptedToRelationship < ActiveRecord::Migration
   def change
     add_column :relationships, :accepted, :boolean, default: false
-
+    add_column :relationships, :declined, :boolean, default: false
   end
 end

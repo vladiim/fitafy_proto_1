a110a32ebbc8dd93af17b9d121a06fde219e7c43
Vlad Ivanovic <vladiim@me.com>
2012-03-08 17:53:53 +1100

    mokey patched WillPaginate::ActionView::LinkRenderer to work with bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,28 @@ module ApplicationHelper
   def big_logo
     image_tag('logo.png', :alt => 'fitafy', :size => '200x200')
   end
+  
+  class ::WillPaginate::ActionView::LinkRenderer
+    protected
+    
+    def html_container(html)
+      tag :div, tag(:ul, html), container_attributes
+    end
+    
+    def page_number(page)
+      tag :li, link(page, page, rel: rel_value(page)), :class => ('active' if page == current_page)
+    end
+    
+    def gap
+      tag :li, link(super, '#'), :class => 'disabled'
+    end
+    
+    def previous_or_next_page(page, text, classname)
+      tag :li, link(text, page || '#'), :class => [classname[0..3], classname, ('disabled' unless page)].join(' ')
+    end
+    
+    def page_navigation_links(pages)
+      will_paginate(pages, :class => 'pagination', :inner_window => 2, :outer_window => 0, :renderer => BootstrapLinkRenderer, :previous_label => '%larr;'.html_safe, :next_label => '&rarr;'.html_safe)
+    end
+  end
 end

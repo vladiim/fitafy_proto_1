410ca23efc52172503d7ab2328f0d6c73cbe7d1c
Vlad Ivanovic <vladiim@me.com>
2012-03-16 12:38:42 +1100

    magration file wasn't in sync with heroku so I've commented the code out, the file: remove_position_from_exercises

--- db/migrate/20120311045502_remove_position_from_exercises.rb
+++ db/migrate/20120311045502_remove_position_from_exercises.rb
@@ -1,9 +1,9 @@
-class RemovePositionFromExercises < ActiveRecord::Migration
-  def up
-    remove_column :exercises, :position
-  end
-
-  def down
-    add_column :exercises, :position, :integer
-  end
-end
+# class RemovePositionFromExercises < ActiveRecord::Migration
+#   def up
+#     remove_column :exercises, :position
+#   end
+# 
+#   def down
+#     add_column :exercises, :position, :integer
+#   end
+# end

93389b89e9cc4d7ef25c9fbbb75f5bc3b56e0dbc
Vlad Ivanovic <vladiim@me.com>
2012-03-16 12:03:23 +1100

    trainer adds themself as a client only if they aren't already a client

--- app/models/user_observer.rb
+++ app/models/user_observer.rb
@@ -13,6 +13,9 @@ class UserObserver < ActiveRecord::Observer
   private
   
   def make_themself_a_client
-    @user.train!(@user) if @user.role == "trainer_role"
+    trainer_already_trains_themself = Relationship.where("trainer_id = ? AND client_id = ?", @user.id, @user.id)
+    unless trainer_already_trains_themself
+      @user.train!(@user) if @user.role == "trainer_role"
+    end
   end
 end
